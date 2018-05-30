27265bf68b13193950b3975d4acd11b2b2c050f0
Tarek C <tarekcourt@protonmail.com>
2018-05-30 19:00:38 +0100

    - intial project - login page design

---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  facebookLogin() {
+
+  }
+
+  instagramLogin() {
+
+  }
+
+  submit() {
+  	
+  }
+
+}
